4c0239a1fc106f6ae22812490c31c739199ed8c0
LiasTif <ltif427@gmail.com>
2025-01-02 16:54:37 +0100

    When installing an object, its full borders are taken into account

--- Assets/Scripts/Game/HUD/BuildingSystem/SetObject.cs
+++ Assets/Scripts/Game/HUD/BuildingSystem/SetObject.cs
@@ -30,22 +30,39 @@ public class SetObject : MonoBehaviour
 
         Vector3Int tilePosition = tilemap.WorldToCell(mouseWorldPos);
 
-        if (IsCellReserved(tilemap, tilePosition))
-        {
-            Debug.Log("Tile placement blocked: Cell is reserved.");
-            return;
-        }
-
         Tile tileToPlace = s.Tile;
 
         Vector2 spriteSize = tileToPlace.sprite.bounds.size * tileToPlace.sprite.pixelsPerUnit;
         int tileWidth = Mathf.CeilToInt(spriteSize.x / 32f);
         int tileHeight = Mathf.CeilToInt(spriteSize.y / 32f);
 
+        if (AreCellsReserved(tilemap, tilePosition, tileWidth, tileHeight))
+        {
+            Debug.Log("Cannot place tile: Area is reserved.");
+            return;
+        }
+
         tilemap.SetTile(tilePosition, tileToPlace);
 
         ReserveTiles(tilemap, tilePosition, tileWidth, tileHeight);
     }
+
+    private bool AreCellsReserved(Tilemap tilemap, Vector3Int anchorPosition, int width, int height)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector3Int offsetPosition = anchorPosition + new Vector3Int(x, y, 0);
+
+                if (IsCellReserved(tilemap, offsetPosition))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
 
     private void ReserveTiles(Tilemap tilemap, Vector3Int anchorPosition, int width, int height)
     {
