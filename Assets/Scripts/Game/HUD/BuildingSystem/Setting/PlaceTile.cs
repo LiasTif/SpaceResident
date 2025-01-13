7afa9932b68c26949b891da22bfbcb1056bea432
LiasTif <ltif427@gmail.com>
2025-01-13 21:06:23 +0100

    The PlaceTile class has been refactored

--- Assets/Scripts/Game/HUD/BuildingSystem/Setting/PlaceTile.cs
+++ Assets/Scripts/Game/HUD/BuildingSystem/Setting/PlaceTile.cs
@@ -44,12 +44,64 @@ public class PlaceTile
         if (_selectedObjectPreview.ObjectTileBase is not ObjectStateTiles stateTiles)
             return _selectedObjectPreview.Tile;
 
-        bool hasTop = HasNeighbor(position + Vector3Int.up);
-        bool hasBottom = HasNeighbor(position + Vector3Int.down);
-        bool hasLeft = HasNeighbor(position + Vector3Int.left);
-        bool hasRight = HasNeighbor(position + Vector3Int.right);
+        var neighbors = GetNeighborsState(position);
 
-        var tileName = (hasTop, hasBottom, hasLeft, hasRight) switch
+        string tileName = GetTileName(neighbors);
+
+        var tile = stateTiles.GetTile(tileName);
+        CalcRotationAngle(neighbors);
+
+        return tile;
+    }
+
+    private void RecalculateNeighborsRotation(Vector3Int position)
+    {
+        Vector3Int[] directions = { Vector3Int.up, Vector3Int.down, Vector3Int.left, Vector3Int.right };
+
+        foreach (var direction in directions)
+        {
+            Vector3Int neighborPosition = position + direction;
+            var neighborTile = _selectedObjectPreview.ObjectTilemap.GetTile(neighborPosition) as Tile;
+
+            if (neighborTile == null)
+                continue;
+
+            var neighbors = GetNeighborsState(neighborPosition);
+
+            var tileName = GetTileName(neighbors);
+
+            if (_selectedObjectPreview.ObjectTileBase is ObjectStateTiles stateTiles)
+            {
+                var tile = stateTiles.GetTile(tileName);
+
+                _selectedObjectPreview.ObjectTilemap.SetTile(neighborPosition, tile);
+                CalcRotationAngle(neighbors);
+                RotateTile(neighborPosition, _selectedObjectPreview.ObjectTilemap);
+            }
+        }
+    }
+
+    private void CalcRotationAngle((bool hasTop, bool hasBottom, bool hasLeft, bool hasRight) neighbors)
+    {
+        _rotationAngle = neighbors switch
+        {
+            (true, true, false, false) => 90f,
+            (true, false, false, true) => 270f,
+            (false, true, true, false) => 90f,
+            (false, true, false, true) => 180f,
+            (true, true, false, true) => 180f,
+            (true, false, true, true) => 270f,
+            (false, true, true, true) => 90f,
+            (true, false, false, false) => 270f,
+            (false, true, false, false) => 90f,
+            (false, false, false, true) => 180f,
+            _ => 0f,
+        };
+    }
+
+    private static string GetTileName((bool hasTop, bool hasBottom, bool hasLeft, bool hasRight) neighbors)
+    {
+        return neighbors switch
         {
             (true, true, false, false) => "straight",
             (false, false, true, true) => "straight",
@@ -68,79 +120,17 @@ public class PlaceTile
             (false, false, false, true) => "deadend",
             _ => "single",
         };
-
-        var tile = stateTiles.GetTile(tileName);
-        CalcRotationAngle(hasTop, hasBottom, hasLeft, hasRight);
-
-        return tile;
     }
 
-    private void CalcRotationAngle(bool hasTop, bool hasBottom, bool hasLeft, bool hasRight)
+    private (bool hasTop, bool hasBottom, bool hasLeft, bool hasRight) GetNeighborsState(Vector3Int position)
     {
-        _rotationAngle = (hasTop, hasBottom, hasLeft, hasRight) switch
-        {
-            (true, true, false, false) => 90f,
-            (true, false, false, true) => 270f,
-            (false, true, true, false) => 90f,
-            (false, true, false, true) => 180f,
-            (true, true, false, true) => 180f,
-            (true, false, true, true) => 270f,
-            (false, true, true, true) => 90f,
-            (true, false, false, false) => 270f,
-            (false, true, false, false) => 90f,
-            (false, false, false, true) => 180f,
-            _ => 0f,                            
-        };
+        return (
+            HasNeighbor(position + Vector3Int.up),
+            HasNeighbor(position + Vector3Int.down),
+            HasNeighbor(position + Vector3Int.left),
+            HasNeighbor(position + Vector3Int.right)
+        );
     }
-
-    private void RecalculateNeighborsRotation(Vector3Int position)
-    {
-        Vector3Int[] directions = { Vector3Int.up, Vector3Int.down, Vector3Int.left, Vector3Int.right };
-
-        foreach (var direction in directions)
-        {
-            Vector3Int neighborPosition = position + direction;
-            var neighborTile = _selectedObjectPreview.ObjectTilemap.GetTile(neighborPosition) as Tile;
-
-            if (neighborTile == null)
-                continue;
-
-            bool hasTop = HasNeighbor(neighborPosition + Vector3Int.up);
-            bool hasBottom = HasNeighbor(neighborPosition + Vector3Int.down);
-            bool hasLeft = HasNeighbor(neighborPosition + Vector3Int.left);
-            bool hasRight = HasNeighbor(neighborPosition + Vector3Int.right);
-
-            var newTileName = (hasTop, hasBottom, hasLeft, hasRight) switch
-            {
-                (true, true, false, false) => "straight",
-                (false, false, true, true) => "straight",
-                (true, false, true, false) => "corner",
-                (true, false, false, true) => "corner",
-                (false, true, true, false) => "corner",
-                (false, true, false, true) => "corner",
-                (true, true, true, true) => "xshaped",
-                (true, true, true, false) => "tshaped",
-                (true, true, false, true) => "tshaped",
-                (true, false, true, true) => "tshaped",
-                (false, true, true, true) => "tshaped",
-                (true, false, false, false) => "deadend",
-                (false, true, false, false) => "deadend",
-                (false, false, true, false) => "deadend",
-                (false, false, false, true) => "deadend",
-                _ => "single",
-            };
-
-            if (_selectedObjectPreview.ObjectTileBase is ObjectStateTiles stateTiles)
-            {
-                var newTile = stateTiles.GetTile(newTileName);
-
-                _selectedObjectPreview.ObjectTilemap.SetTile(neighborPosition, newTile);
-                CalcRotationAngle(hasTop, hasBottom, hasLeft, hasRight);
-                RotateTile(neighborPosition, _selectedObjectPreview.ObjectTilemap);
-            }
-        }
-    }
-
 
     private bool HasNeighbor(Vector3Int position)
     {
